e642827417e8e41c003bb9cf02a6f87e1589cfdc
Georgi Balbuzanov <rehash@Georgis-MacBook-Pro.local>
2018-04-05 09:40:05 +0300

    added first alpha version 13

--- src/Http/Middleware/PhantomMiddleware.php
+++ src/Http/Middleware/PhantomMiddleware.php
@@ -6,35 +6,38 @@ use Closure;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
-class PhantomMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-	    \Session::put('phantom.modules.current',$request->route()->getName());
+class PhantomMiddleware {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Closure $next
+	 * @return mixed
+	 */
+	public function handle(Request $request, Closure $next) {
+		if (!auth()->user()) {
+			return redirect('/login');
+		}
 
-	    \Session::put('phantom.modules.main','admin');
-	    $lang=explode('-',$request->server('HTTP_ACCEPT_LANGUAGE'));
-	    if (in_array($lang[0],config('app.locales'))) {
-		    \App::setLocale($lang[0]);
-		    Carbon::setLocale($lang[0]);
-		    \Session::forget('locale');;
-	    }
-	    else if (\Session::has('locale')) {
-		    \App::setLocale(\Session::get('locale'));
-		    Carbon::setLocale(\Session::get('locale'));
-		    \Session::put('locale', $request->lang);;
-	    }
-	    if ($request->lang) {
-		    \Session::put('locale', $request->lang);;
-		    \App::setLocale($request->lang);
-	    }
-        return $next($request);
-    }
+		\Session::put('phantom.modules.current', $request->route()->getName());
+
+		\Session::put('phantom.modules.main', 'admin');
+		$lang = explode('-', $request->server('HTTP_ACCEPT_LANGUAGE'));
+		if (in_array($lang[0], config('app.locales'))) {
+			\App::setLocale($lang[0]);
+			Carbon::setLocale($lang[0]);
+			\Session::forget('locale');;
+		}
+		else if (\Session::has('locale')) {
+			\App::setLocale(\Session::get('locale'));
+			Carbon::setLocale(\Session::get('locale'));
+			\Session::put('locale', $request->lang);;
+		}
+		if ($request->lang) {
+			\Session::put('locale', $request->lang);;
+			\App::setLocale($request->lang);
+		}
+
+		return $next($request);
+	}
 }
